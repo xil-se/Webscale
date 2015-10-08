29db20dc67813cb48590d29073dc7f591db02486
David Plassmann <dp@one.com>
2015-10-08 16:17:11 +0200

    Fixed a bit of style and added robots option

--- ReverseProxy/proxy.go
+++ ReverseProxy/proxy.go
@@ -25,6 +25,7 @@ type Config struct {
 	ListenAddress string
 	BackendScheme string
 	BackendHost   string
+	ReplaceRobots bool
 	Match         []Match
 }
 
@@ -39,54 +40,57 @@ var confPath string
 var vers bool
 var version = "Not set"
 
+const robotstxt = `User-agent: *
+Disallow: /
+`
+
 func (t *myTransport) RoundTrip(request *http.Request) (*http.Response, error) {
 	response, err := http.DefaultTransport.RoundTrip(request)
 	handling := "-"
-	if val, ok := response.Header["Content-Type"]; ok {
-		ctype := val[0]
+	if strings.Contains(response.Header.Get("Content-Type"), "text/") {
 
-		if strings.Contains(ctype, "text/") {
+		bin, readErr := ioutil.ReadAll(response.Body)
+		if readErr != nil {
+			log.Printf("Error reading: %s", readErr)
+			return nil, readErr
+		}
 
-			bin, readErr := ioutil.ReadAll(response.Body)
-			if readErr != nil {
-				return nil, err
-			}
-
-			body := bin
-			var replace []byte
-			for _, v := range conf.Match {
-				if !v.DisableTmp {
-					tmpl, err := template.New("replace").Parse(v.Replace)
-					if err != nil {
-						log.Printf("Could not parse template: %s with error: %s", v.Replace, err)
-						continue
-					}
-
-					buf := new(bytes.Buffer)
-					err = tmpl.Execute(buf, request)
-					if err != nil {
-						log.Printf("Could not parse template: %s with error: %s", v.Replace, err)
-						continue
-					}
-					replace = buf.Bytes()
-				} else {
-					replace = []byte(v.Replace)
+		body := bin
+		var replace []byte
+		for _, v := range conf.Match {
+			if !v.DisableTmp {
+				tmpl, err := template.New("replace").Parse(v.Replace)
+				if err != nil {
+					log.Printf("Could not parse template: %s with error: %s", v.Replace, err)
+					continue
 				}
 
-				regx := regexp.MustCompile(v.Search)
-				body = regx.ReplaceAll(body, replace)
+				buf := new(bytes.Buffer)
+				err = tmpl.Execute(buf, request)
+				if err != nil {
+					log.Printf("Could not parse template: %s with error: %s", v.Replace, err)
+					continue
+				}
+				replace = buf.Bytes()
+			} else {
+				replace = []byte(v.Replace)
 			}
 
-			response.Header.Set("Content-Length", strconv.Itoa(len(body)))
-			handling = "handled"
-			response.Body = ioutil.NopCloser(bytes.NewReader(body))
+			regx := regexp.MustCompile(v.Search)
+			body = regx.ReplaceAll(body, replace)
 		}
+
+		response.Header.Set("Content-Length", strconv.Itoa(len(body)))
+		handling = "handled"
+		response.Body = ioutil.NopCloser(bytes.NewReader(body))
 	}
 
 	log.Printf("%s %s %s %s %q %d %s\n", request.RemoteAddr, request.Host, request.Method, request.URL, request.Header.Get("User-Agent"), response.StatusCode, handling)
 	return response, err
 }
-
+func robots(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, robotstxt)
+}
 func init() {
 	flag.BoolVar(
 		&vers,
@@ -123,10 +127,11 @@ func main() {
 	}
 
 	proxy := &httputil.ReverseProxy{Director: director, Transport: ts}
-	server := http.Server{
-		Addr:    conf.ListenAddress,
-		Handler: proxy,
+	if conf.ReplaceRobots {
+		http.Handle("/robots.txt", http.HandlerFunc(robots))
 	}
+	http.Handle("/", proxy)
+
 	fmt.Printf("Listening on %s\n", conf.ListenAddress)
-	server.ListenAndServe()
+	log.Fatal(http.ListenAndServe(conf.ListenAddress, nil))
 }
